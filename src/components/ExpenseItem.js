292b9626dfd9207db7deaa5249f5437589292fad
Mobinzaid12 <mobinzaidi2015@gmail.com>
2023-10-17 13:41:28 -0700

    expense

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import "./ExpenseItem.css"
+import ExpenseData from "./ExpenseDate";
+import Card from "./Card";
+
+function ExpenseItem(props) {
+
+    return (
+        <Card className="expense-item">
+            <ExpenseData date={props.date} />
+            <div className="expense-item__description">
+                <h2 className="expense-item h2">{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
+            </div>
+        </Card>
+    );
+
+}
+
+export default ExpenseItem;
